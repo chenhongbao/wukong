d50ec1648a887cede1c7c27f3b8e92f441cfd0e9
陈宏葆 <chenhongbao@outlook.com>
2020-06-25 15:29:45 +0800

    Test TransactionList.

--- src/main/com/nabiki/wukong/OP.java
+++ src/main/com/nabiki/wukong/OP.java
@@ -1,11 +1,43 @@
+/*
+ * Copyright (c) 2020 Hongbao Chen <chenhongbao@outlook.com>
+ *
+ * Licensed under the  GNU Affero General Public License v3.0 and you may not use
+ * this file except in compliance with the  License. You may obtain a copy of the
+ * License at
+ *
+ *                    https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Permission is hereby  granted, free of charge, to any  person obtaining a copy
+ * of this software and associated  documentation files (the "Software"), to deal
+ * in the Software  without restriction, including without  limitation the rights
+ * to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+ * copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+ * IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+ * FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+ * AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.nabiki.wukong;
 
 import java.io.*;
 
 public class OP {
     /**
-     * Get a deep copy of the specified object. The method does the deep copy
-     * via serialize the object to a byte array that then deserializes from.
+     * Get a deep copy of the specified object. The method first serializes the
+     * object to a byte array and then recover a new object from it.
+     *
+     * <p>The object to be copied must implement {@link Serializable}, or the method
+     * fails and returns {@code null}.
+     * </p>
      *
      * @param copied the specified object to be deeply copied
      * @param <T> generic type of a copied object
@@ -18,6 +50,8 @@ public class OP {
             return (T) new ObjectInputStream(
                     new ByteArrayInputStream(bo.toByteArray())).readObject();
         } catch (IOException | ClassNotFoundException ignored) {
+            System.err.println(ignored.getMessage());
+            ignored.printStackTrace();
             return null;
         }
     }
